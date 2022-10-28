064942b24ebe571626020c92ab21f6735bdadea9
Tyoma <chaplinskyartem@gmail.com>
2022-10-28 15:41:13 +0300

    db compatoBILLYty update

--- DB_create.sql
+++ DB_create.sql
@@ -5,14 +5,12 @@ role varchar(10) not null default 'user',
 accesstoken varchar(255),
 refreshtoken varchar(255));
 
-insert into user1( login, password, role) values ( 'admin', 'admin', 'admin');
 
 create table goods(
 id bigint not null primary key,
 name varchar(30) not null,
 cost numeric(10,2) not null,
 quantity numeric(10,0) default 0);
-insert into products1( name, cost, quantity) values ( 'Apple', '10.2', '1000');
 
 create table purchase_history1(
 nameofuser varchar(10) not null,
