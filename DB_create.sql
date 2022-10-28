c1ae81dfd71d45756dee487bcd48ced9cb67e28d
Tyoma <chaplinskyartem@gmail.com>
2022-10-28 18:09:42 +0300

    add inser .sql

--- DB_create.sql
+++ DB_create.sql
@@ -5,6 +5,7 @@ role varchar(10) not null default 'user',
 accesstoken varchar(255),
 refreshtoken varchar(255));
 
+insert into users (login,password,role) values ('admin','admin','admin');
 
 create table goods(
 id bigint not null primary key,
@@ -12,7 +13,9 @@ name varchar(30) not null,
 cost numeric(10,2) not null,
 quantity numeric(10,0) default 0);
 
-create table purchase_history1(
+insert into goods (name,cost,quantity) values ('apple', 1.2, 100)
+
+create table purchase(
 nameofuser varchar(10) not null,
 nameofgoods varchar(30) not null,
 quantity numeric(10,0) not null,
